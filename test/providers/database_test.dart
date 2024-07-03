e7c8e136293b320ffbd9dab4b8966b96f47119fa
Farezi <fpcrezende@gmail.com>
2024-07-03 14:07:27 -0400

    Organized database_test.dart

--- test/providers/database_test.dart
+++ test/providers/database_test.dart
@@ -18,15 +18,18 @@ Future main() async {
       String coordTableCreateTest =
           'CREATE TABLE coordinatesTest(id TEXT PRIMARY KEY, latitude DOUBLE NOT NULL, longitude DOUBLE NOT NULL, runId TEXT NOT NULL, FOREIGN KEY(runId) REFERENCES runDataTest(id) ON DELETE CASCADE);';
 
-      db = await openDatabase(inMemoryDatabasePath, version: 1,
-          onCreate: (db, version) async {
-        db.execute(
-          runDataTableCreateTest,
-        );
-        db.execute(
-          coordTableCreateTest,
-        );
-      });
+      db = await openDatabase(
+        inMemoryDatabasePath,
+        version: 1,
+        onCreate: (db, version) async {
+          db.execute(
+            runDataTableCreateTest,
+          );
+          db.execute(
+            coordTableCreateTest,
+          );
+        },
+      );
       await db.execute('PRAGMA foreign_keys = ON');
     });
 
@@ -81,22 +84,32 @@ Future main() async {
     });
 
     test('inserting into runDataTest', () async {
-      await db.insert('runDataTest', {
-        'email': runData.email,
-        'id': runData.id,
-      });
+      await db.insert(
+        'runDataTest',
+        {
+          'email': runData.email,
+          'id': runData.id,
+        },
+      );
 
       List<Map<String, Object?>> runDataExists = await db.query('runDataTest');
       expect(runDataExists, hasLength(1));
     });
 
     test('deleting from runDataTest', () async {
-      await db.insert('runDataTest', {
-        'email': runData.email,
-        'id': runData.id,
-      });
+      await db.insert(
+        'runDataTest',
+        {
+          'email': runData.email,
+          'id': runData.id,
+        },
+      );
 
-      await db.delete('runDataTest', where: 'id = ?', whereArgs: [runData.id]);
+      await db.delete(
+        'runDataTest',
+        where: 'id = ?',
+        whereArgs: [runData.id],
+      );
 
       List<Map<String, Object?>> runDataExists = await db.query('runDataTest');
       expect(runDataExists, hasLength(0));
@@ -110,10 +123,13 @@ Future main() async {
     });
 
     test('inserting into coordinatesTest, runData exists', () async {
-      await db.insert('runDatatest', {
-        'email': runData.email,
-        'id': runData.id,
-      });
+      await db.insert(
+        'runDatatest',
+        {
+          'email': runData.email,
+          'id': runData.id,
+        },
+      );
 
       List<Map<String, Object?>> runDataQuery = await db.query('runDataTest');
       List<bool> runIdExists = [];
@@ -126,12 +142,15 @@ Future main() async {
       expect(runIdExists, contains(true));
 
       if (runIdExists.isNotEmpty && runIdExists.contains(true)) {
-        await db.insert('coordinatesTest', {
-          'id': mockCoordinates.first.id,
-          'latitude': mockCoordinates.first.latitude,
-          'longitude': mockCoordinates.first.longitude,
-          'runId': mockCoordinates.first.runData.id,
-        });
+        await db.insert(
+          'coordinatesTest',
+          {
+            'id': mockCoordinates.first.id,
+            'latitude': mockCoordinates.first.latitude,
+            'longitude': mockCoordinates.first.longitude,
+            'runId': mockCoordinates.first.runData.id,
+          },
+        );
       }
 
       List<Map<String, Object?>> runDataExists =
@@ -150,12 +169,15 @@ Future main() async {
       expect(runIdExists, hasLength(0));
 
       if (runIdExists.isNotEmpty && runIdExists.contains(true)) {
-        await db.insert('coordinatesTest', {
-          'id': mockCoordinates.first.id,
-          'latitude': mockCoordinates.first.latitude,
-          'longitude': mockCoordinates.first.longitude,
-          'runId': mockCoordinates.first.runData.id,
-        });
+        await db.insert(
+          'coordinatesTest',
+          {
+            'id': mockCoordinates.first.id,
+            'latitude': mockCoordinates.first.latitude,
+            'longitude': mockCoordinates.first.longitude,
+            'runId': mockCoordinates.first.runData.id,
+          },
+        );
       }
 
       List<Map<String, Object?>> runDataExists =
@@ -165,10 +187,13 @@ Future main() async {
 
     // delete coodinate, mantains rundata table
     test('deleting from coordinatesTest', () async {
-      await db.insert('runDatatest', {
-        'email': runData.email,
-        'id': runData.id,
-      });
+      await db.insert(
+        'runDatatest',
+        {
+          'email': runData.email,
+          'id': runData.id,
+        },
+      );
 
       List<Map<String, Object?>> runDataQuery = await db.query('runDataTest');
       List<bool> runIdExists = [];
@@ -178,19 +203,25 @@ Future main() async {
       }
 
       if (runIdExists.isNotEmpty && runIdExists.contains(true)) {
-        await db.insert('coordinatesTest', {
-          'id': mockCoordinates.first.id,
-          'latitude': mockCoordinates.first.latitude,
-          'longitude': mockCoordinates.first.longitude,
-          'runId': mockCoordinates.first.runData.id,
-        });
+        await db.insert(
+          'coordinatesTest',
+          {
+            'id': mockCoordinates.first.id,
+            'latitude': mockCoordinates.first.latitude,
+            'longitude': mockCoordinates.first.longitude,
+            'runId': mockCoordinates.first.runData.id,
+          },
+        );
       }
 
       expect(await db.query('runDataTest'), hasLength(1));
       expect(await db.query('coordinatesTest'), hasLength(1));
 
-      await db.delete('coordinatesTest',
-          where: 'id = ?', whereArgs: [mockCoordinates.first.id]);
+      await db.delete(
+        'coordinatesTest',
+        where: 'id = ?',
+        whereArgs: [mockCoordinates.first.id],
+      );
 
       List<Map<String, Object?>> coordinatesExist =
           await db.query('coordinatesTest');
@@ -202,10 +233,13 @@ Future main() async {
 
     // delete from rundatatest, all coordinates are deleted
     test('deleting coordinatesTest, from runDataTest', () async {
-      await db.insert('runDatatest', {
-        'email': runData.email,
-        'id': runData.id,
-      });
+      await db.insert(
+        'runDatatest',
+        {
+          'email': runData.email,
+          'id': runData.id,
+        },
+      );
 
       List<Map<String, Object?>> runDataQuery = await db.query('runDataTest');
       List<bool> runIdExists = [];
@@ -215,18 +249,25 @@ Future main() async {
       }
 
       if (runIdExists.isNotEmpty && runIdExists.contains(true)) {
-        await db.insert('coordinatesTest', {
-          'id': mockCoordinates.first.id,
-          'latitude': mockCoordinates.first.latitude,
-          'longitude': mockCoordinates.first.longitude,
-          'runId': mockCoordinates.first.runData.id,
-        });
+        await db.insert(
+          'coordinatesTest',
+          {
+            'id': mockCoordinates.first.id,
+            'latitude': mockCoordinates.first.latitude,
+            'longitude': mockCoordinates.first.longitude,
+            'runId': mockCoordinates.first.runData.id,
+          },
+        );
       }
 
       expect(await db.query('runDataTest'), hasLength(1));
       expect(await db.query('coordinatesTest'), hasLength(1));
 
-      await db.delete('runDataTest', where: 'id = ?', whereArgs: [runData.id]);
+      await db.delete(
+        'runDataTest',
+        where: 'id = ?',
+        whereArgs: [runData.id],
+      );
 
       List<Map<String, Object?>> coordinatesExist =
           await db.query('coordinatesTest');
